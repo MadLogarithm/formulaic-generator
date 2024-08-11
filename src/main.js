aa1f2fcf70baddd34ebf896b0f47b7b3f07f2a23
MadLogarithm <1141519626@qq.com>
2024-08-11 22:37:35 +0800

    init(): 安装依赖

--- src/main.js
+++ src/main.js
@@ -2,5 +2,10 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import ElementPlus from 'element-plus'
+import router from './router'
+import 'element-plus/dist/index.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(ElementPlus)
+app.use(router)
